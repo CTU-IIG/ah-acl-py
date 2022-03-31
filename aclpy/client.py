a60d918a6f8e0d0881020e60508df3fc7e82414d
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 17:47:51 +0200

    Add 'orchestrate' to client

--- aclpy/client.py
+++ aclpy/client.py
@@ -3,13 +3,17 @@
 """Arrowhead Client class.
 """
 
+import json
+
+from aclpy.connector.connectorabc import ConnectorABC
+from aclpy.messages import *
 from aclpy.service import ArrowheadService
 from aclpy.system import ArrowheadSystem
 
 
 class ArrowheadClient(ArrowheadSystem):
 
-    def __init__(self, name: str, address: str, port: int, p12file: str, p12pass: str, pubfile: str, cafile: str):
+    def __init__(self, name: str, address: str, port: int, p12file: str, p12pass: str, pubfile: str, cafile: str, connector: ConnectorABC):
         # Read pubkey first
         with open(pubfile, "r") as f:
             pubkey = f.read()
@@ -20,6 +24,7 @@ class ArrowheadClient(ArrowheadSystem):
         self.p12pass = p12pass
         self.pubfile = pubfile
         self.cafile = cafile
+        self.connector = connector
 
 
     def register_service(self, service: ArrowheadService) -> bool:
@@ -32,3 +37,21 @@ class ArrowheadClient(ArrowheadSystem):
 
     def register_system(self) -> bool:
         pass
+
+
+    def orchestrate(self, service: ArrowheadService) -> ArrowheadSystem:
+        msg = build_orchestration_request("HTTP-INSECURE-JSON", self, service)
+
+        status_code, response = self.connector.orchestrate(self, msg)
+
+        if (status_code >= 400):
+            print ("Client is not authorized for communication with the Orchestrator.", file=sys.stderr)
+            return None
+        else:
+            system = json.loads(response).get("response")[0].get("provider")
+            return ArrowheadSystem(
+                address = system.get("address"),
+                port = system.get("port"),
+                name = system.get("name"),
+                pubkey = system.get("authenticationInfo"),
+            )
