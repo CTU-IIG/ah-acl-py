9ceedcfb2f8cc27c9beac32f9a01a20aff93b46b
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 13:19:49 +0200

    Create empty messages with stubs

---
+++ aclpy/messages.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# messages.py
+"""Definition of various Arrowhead-related messages.
+"""
+
+def build_register_service():
+    pass
+
+
+def build_unregister_service():
+    pass
+
+
+def build_register_system():
+    pass
