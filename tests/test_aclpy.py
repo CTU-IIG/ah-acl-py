c767c7d9a2e43081c4796653d46c29512baf4353
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 16:07:00 +0200

    Add package import test

---
+++ tests/test_aclpy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# test_aclpy.py
+"""Test the package by import.
+"""
+
+import unittest
+
+import aclpy.client
+import aclpy.messages
+import aclpy.service
+import aclpy.system
+
+class TestPackage(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
