de9518c1008f641eff25267614dccb6ae7f09e09
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 16:16:27 +0200

    Implement 'build_register_service'

--- aclpy/messages.py
+++ aclpy/messages.py
@@ -3,8 +3,63 @@
 """Definition of various Arrowhead-related messages.
 """
 
-def build_register_service():
-    pass
+from typing import Dict
+
+from aclpy.system import ArrowheadSystem
+from aclpy.service import ArrowheadService
+
+
+def build_register_service(
+        interface: str,
+        system: ArrowheadSystem,
+        service: ArrowheadService,
+    ) -> Dict[str, any]:
+    return {
+        # *Which interface do we use?
+        # The convention (name pattern is) PROTOCOL-SECURE/INSECURE-DATA_FORMAT
+        # I assume that SECURE = encrypted by the TOKEN/CERTIFICATE
+        "interfaces": [
+            interface,
+        ],
+
+        # *Who are we?
+        # Here we introduce the system providing the service.
+        # 'systemName' should be same as the name in the certificate.
+        #   - Otherwise, we get an SSL error.
+        # 'authenticationInfo' is required with 'CERTIFICATE' and 'TOKEN'
+        #   - For 'CERTIFICATE' I put there public key (so it should be asymmetric encryption).
+        # 'address' is an IP address / name? of the server
+        # 'port' is port used for the communication
+        "providerSystem": {
+            "systemName": system.name,
+            "authenticationInfo": system.pubkey,
+            "address": system.address,
+            "port": system.port,
+        },
+
+        # *Which service we provide?
+        "serviceDefinition": service.name,
+
+        # Security info (probably just showing what can be used for authorization?)
+        #  - Default is 'NOT_SECURE', other options are: 'CERTIFICATE' and 'TOKEN'
+        "secure": "CERTIFICATE",
+
+        # Version of the service
+        "version": service.version,
+
+        ## Other parts we do not use (even though some of them are mandatory*):
+
+        # *URI of the service
+        # "serviceUri": "string",
+
+        # Service is available until this UTC timestamp
+        # "endOfValidity": "string",
+
+        # Various optional metadata
+        # "metadata": {
+        #     "additionalProperty1": "string",
+        # },
+    }
 
 
 def build_unregister_service():
