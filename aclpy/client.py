9cf74d72b6b10d75b39f6b620c8eee7740e06fe3
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 12:57:03 +0200

    Add function stubs

--- aclpy/client.py
+++ aclpy/client.py
@@ -3,5 +3,22 @@
 """Arrowhead Client class.
 """
 
+from aclpy.service import ArrowheadService
+
+
 class ArrowheadClient(object):
-    pass
+
+    def __init__(self):
+        pass
+
+
+    def register_service(self, service: ArrowheadService) -> bool:
+        pass
+
+
+    def unregister_service(self, service: ArrowheadService) -> bool:
+        pass
+
+
+    def register_system(self) -> bool:
+        pass
