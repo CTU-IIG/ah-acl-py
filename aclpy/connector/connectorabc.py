7360153d3d797333714d9edb40ec80bf99b67c35
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 17:19:45 +0200

    Add abstract class for connector

---
+++ aclpy/connector/connectorabc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# connectorabc.py
+"""Abstract class for Connectors.
+"""
+
+from typing import Dict, Tuple
+
+from aclpy.server import ArrowheadServer
+from aclpy.system import ArrowheadSystem
+
+
+class ConnectorABC(object):
+
+    def __init__(self, server: ArrowheadServer):
+        super(ConnectorABC, self).__init__()
+
+        self.server = server
+
+
+    @abstractmethod
+    def orchestration(self, system: ArrowheadSystem, message: Dict[str, any]) -> Tuple(int, str):
+        pass
