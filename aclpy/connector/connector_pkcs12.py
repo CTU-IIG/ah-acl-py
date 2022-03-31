349f366cbb4b81c594f6db2b36d107a81ac81a1b
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-03-31 17:29:28 +0200

    Add PKCS12 version of connector

---
+++ aclpy/connector/connector_pkcs12.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# connector_pkcs12.py
+"""Connector / interface to Arrowhead Core using .p12 certificates.
+"""
+import requests_pkcs12
+
+from typing import Dict, Tuple
+
+from aclpy.connector.connectorabc import ConnectorABC
+from aclpy.server import ArrowheadServer
+from aclpy.system import ArrowheadSystem
+
+
+class ArrowheadConnector(ConnectorABC):
+
+    def orchestration(self, system: ArrowheadSystem, message: Dict[str, any]) -> Tuple[int, str]:
+        res = requests_pkcs12.post(
+            self.server.get_url("orchestrator") + "orchestration",
+            json = message,
+            pkcs12_filename = system.p12file,
+            pkcs12_password = system.p12pass,
+            verify = system.cafile,
+        )
+
+        return (res.status_code, res.text)
