a6e80d0b929ee5726473e0a194924e75c172d9f4
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-04-04 10:40:07 +0200

    Docstring service

--- aclpy/service.py
+++ aclpy/service.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 # service.py
-"""Service class for Arrowhead.
+"""Arrowhead service definition for the library.
 """
 
 class ArrowheadService(object):
+    """ArrowheadService class to store configuration about a service.
+
+    Attributes:
+    name (str) -- name of the service
+    version (int) -- version of the service, 1 by default
+    """
 
     __slots__ = ["name", "version"]
 
-    def __init__(self, name: str, version: int = 1):
+    def __init__(self, *,
+            name: str,
+            version: int = 1
+    ):
+        """Initialize ArrowheadService class."""
         super(ArrowheadService, self).__init__()
 
         self.name = name
