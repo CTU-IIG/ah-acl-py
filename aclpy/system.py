4da6c1247a060f062a793f64fc4fc21f450960bc
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-04-04 10:42:49 +0200

    Docstring system

--- aclpy/system.py
+++ aclpy/system.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 # system.py
-"""System class for Arrowhead.
+"""Arrowhead system definition for the library.
 """
 
 class ArrowheadSystem(object):
+    """ArrowheadSystem class to store information about the system.
+
+    Attributes:
+    name (str) -- name of the system
+    address (str) -- address IP / name of the system
+    port (int) -- port that the system is attached to
+    pubkey (str) -- public key of the system, "" by default
+
+    Note: When using the system as connect only, feel free to use port 0.
+    """
 
     __slots__ = ["name", "address", "port", "pubkey"]
 
-    def __init__(self, name: str, address: str, port: int, pubkey: str = ""):
+    def __init__(self, *,
+            name: str,
+            address: str,
+            port: int,
+            pubkey: str = ""
+    ):
+        """Initialize ArrowheadSystem class."""
         super(ArrowheadSystem, self).__init__()
 
         self.name = name
