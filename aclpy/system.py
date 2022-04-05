26ec952c1c21317f26c58e12edfa106ec122da3a
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2022-04-05 08:16:40 +0200

    Add system attributes from Arrowhead Core

--- aclpy/system.py
+++ aclpy/system.py
@@ -11,17 +11,24 @@ class ArrowheadSystem(object):
     address (str) -- address IP / name of the system
     port (int) -- port that the system is attached to
     pubkey (str) -- public key of the system, "" by default
+    id (int) -- identification number of the system, default -1 (not known)
+    created_at (str) -- timestamp of system creation, default ""
+    updated_at (str) -- timestamp of the last system update, default ""
 
     Note: When using the system as connect only, feel free to use port 0.
+    Note: Timestamp is given as '%Y-%m-%d %H-%M-%S'.
     """
 
-    __slots__ = ["name", "address", "port", "pubkey"]
+    __slots__ = ["name", "address", "port", "pubkey", "id", "created_at", "updated_at"]
 
     def __init__(self, *,
             name: str,
             address: str,
             port: int,
-            pubkey: str = ""
+            pubkey: str = "",
+            id: int = -1,
+            created_at: str = "",
+            updated_at: str = "",
     ):
         """Initialize ArrowheadSystem class."""
         super(ArrowheadSystem, self).__init__()
@@ -30,3 +37,6 @@ class ArrowheadSystem(object):
         self.address = address
         self.port = port
         self.pubkey = pubkey
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
